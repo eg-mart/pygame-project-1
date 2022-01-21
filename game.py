a721d65d8ef04b586165cbd9039046987a9da752
fire9900 <37242708+fire9900@users.noreply.github.com>
2022-01-21 21:39:24 +0400

    Начало меню
    Добавил отрисовку стартового окна перед загрузкой уровней

--- game.py
+++ game.py
@@ -2,6 +2,21 @@ import sys
 
 import pygame
 import configparser
+import os
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('sprites', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 class Game:
@@ -13,12 +28,28 @@ class Game:
         self.screen = pygame.display.set_mode(self.size)
         self.fps = int(config['GRAPHICS']['fps'])
         self.frame_clock = pygame.time.Clock()
+        self.in_start_screen = True
 
         self.render_queue = []  # список всех групп, которые будут отрисованы в следующий кадр
 
         self.subscribers = dict()  # словарь вида {event_type: callback} для подписчиков на события
 
     def run(self):
+        fon = pygame.transform.scale(load_image('fon.jpg'), self.size)
+
+        # Цикл отрисовки для стартового окна
+        while self.in_start_screen:
+            self.frame_clock.tick(self.fps)
+            for event in pygame.event.get():
+                self.screen.blit(fon, (0, 0))
+                if event.type == pygame.QUIT:
+                    self.terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    self.in_start_screen = False  # начинаем игру
+                self.load_start_screen(self.screen, fon)
+                pygame.display.flip()
+
         while True:
             self.frame_clock.tick(self.fps)
             self.screen.fill(pygame.Color('black'))
@@ -52,3 +83,24 @@ class Game:
 
     def unsubscribe(self, event_type, callback):
         self.subscribers[event_type].remove(callback)
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
+
+    def load_start_screen(self, screen, fon):
+        intro_text = ["ЗАСТАВКА", "",
+                      "Кликни чтобы начать",
+                      "Нужно найти другой способ",
+                      "отрисовки этого текста"]
+        screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
