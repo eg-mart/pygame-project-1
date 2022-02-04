6fb250de23ee0ba822075a6033e78b3cf740d166
eg-mart <marteg71@gmail.com>
2022-02-04 05:24:11 +0400

    Починил загрузку уровня

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import pygame
 
 
 def load_level(e):
+    g.clear_render()
+    g.unsubscribe(pygame.USEREVENT + 1, start_game)
+    g.unsubscribe(pygame.USEREVENT + 2, load_level)
+
     lvl.load()
+    g.set_camera_target(lvl.character)
+    g.render(lvl)
+    g.subscribe(pygame.KEYDOWN, lambda e: open_start_screen(g, lvl) if e.key == pygame.K_ESCAPE else None)
 
 
 def open_start_screen(*args, **kwargs):
